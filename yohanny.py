0ba7b018f5873e1796699ac58f63227c5f283243
GDubay <131362003+GDubay@users.noreply.github.com>
2024-03-13 09:24:55 -0300

    Add files via upload

---
+++ yohanny.py
@@ -0,0 +1,4 @@
+noob=int(input("Digite um numero para tanuada"))
+for numero in range (1,11):
+ tabuada = numero * noob
+ print(noob,"X",numero,"=",tabuada)
---
+++ yoyo.py
@@ -0,0 +1,3 @@
+print("Os numeros de 10 a 1 em ordem decrescente sao:")
+for i in  range(10,0,-1):
+    print(i)
